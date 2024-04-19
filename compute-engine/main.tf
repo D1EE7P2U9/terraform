ed9c3e883fd98a67758627d5af358d0ec5bc94eb
D1EE7P2U9 <108419163+D1EE7P2U9@users.noreply.github.com>
2024-04-19 17:03:14 +0530

    Create main.tf

---
+++ compute-engine/main.tf
@@ -0,0 +1,22 @@
+
+resource "google_compute_instance" "vm-from-terraform"{
+  name         = "your-instance-name"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+}
